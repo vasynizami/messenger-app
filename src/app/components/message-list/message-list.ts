402bce0e5d009f31e84e4dee520fd199dc5390b5
Vasilia Nizami <vasilianizami@gmail.com>
2025-06-21 00:01:23 -0400

    list styles finished

--- src/app/components/message-list/message-list.ts
+++ src/app/components/message-list/message-list.ts
@@ -1,13 +1,16 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IMessage } from '../../interfaces/message';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-message-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './message-list.html',
   styleUrl: './message-list.scss',
 })
 export class MessageList {
   @Input() messages$!: Observable<IMessage[]>;
+
+  ngOnInit() {}
 }
